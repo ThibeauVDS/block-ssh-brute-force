32776614a8765c648eb4da2d2c34b68891ee310c
Student AP <student@localhost.localdomain>
2021-04-25 06:04:39 -0700

    t is versie 1.0.2 omdat ik vastloop voor de invalid user te vinden

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -8,8 +8,10 @@ file1 = open("sshdlog", "r")
 #ik maak van file1 iets gemakkelijker en ik zeg daar ook voor te lezen en de lijnen te splitten zodat het niet verticaal kwam (was bij mij een voorval)
 readfile = file1.read().split('/n')
 #ik zoek achter invalid user in mijn file (dit is nog niet volledig, ik zit hier vast.
+
 for line in readfile:
     if search in line:
         print()
+
 #ik close de file.
 file1.close()
