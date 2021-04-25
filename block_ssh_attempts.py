145ac8a9c84e13058e7b6b2a9fd53bac24efe71e
Student AP <student@localhost.localdomain>
2021-04-25 05:55:10 -0700

    versie 1.0.2, vast gelopen in de for.

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
-test = open("sshdlog", "r")
-print(test.read())
-test.close()
+#probeer te zoeken achter een string in dit geval invalid user
+search = "Invalid user"
+
+#ik open de file en open deze op read.
+file1 = open("sshdlog", "r")
+#ik maak van file1 iets gemakkelijker en ik zeg daar ook voor te lezen en de lijnen te splitten zodat het niet verticaal kwam (was bij mij een voorval)
+readfile = file1.read().split('/n')
+#ik zoek achter invalid user in mijn file (dit is nog niet volledig, ik zit hier vast.
+for line in readfile:
+    if search in line:
+        print()
+#ik close de file.
+file1.close()
